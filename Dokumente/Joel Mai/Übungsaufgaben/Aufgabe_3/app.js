43af66df40e1dbd6707c86886d69f97fd4f68cdd
Joel Mai <joel@maispace.de>
2019-11-02 14:37:58 +0100

    Aufgabe 3 done

---
+++ Dokumente/Joel Mai/Übungsaufgaben/Aufgabe_3/app.js
@@ -0,0 +1,54 @@
+// Einbindung von dem eigenen 'cities_module' Modul
+const cm = require('./cities_module'); 
+
+cm.getCities().then(() => {                                         // Liefert die Liste aller Städte
+    cm.findCity('Düren').then(() => {                               // Liefert die Gewünschte Stadt
+        cm.deleteCity('Düren').then(() => {                         // Löscht die gewünschte Stadt aus der dem JSON File
+            cm.getCities().then(() => {
+                cm.addCity('Düren', '90733', 'NRW').then(() => {    // Fügt die Stadt wieder dem JSON File hinzu
+                    cm.getCities().then(() => {
+                        printUsers();                               // Aufgabe 4 - 
+                    });
+                });
+            });
+        });
+    });
+});
+// Einbinden des File-System Modul
+const fs = require('fs');
+
+// Ausgabe aller Nutzer
+function printUsers() {
+    console.log(' ');                                                               // Ausgabentrennung
+
+    var cities;                                                                     // Städte Variable
+    var users;                                                                      // Nutzer Variable
+
+    try {
+        fs.readFile('cities.json', 'utf8', ( err , data ) => {             // Einlesen der Städte 
+            if ( err ) throw( err );                                                // Bei Fehler Abbruch
+            cities = JSON.parse(data);                                              // Übergeben an die Variable
+
+            fs.readFile('users.json', 'utf8', ( err , data ) => {          // Einlesen der Nutzer
+                if ( err ) throw( err );                                            // Bei Fehler Abbruch
+                users = JSON.parse(data);                                           // Übergeben an die Variable
+
+                users.forEach( ( user ) => {                                      // Für jeden Nutzer
+                    let einwohnerzahl;                                              // temporäre Variable für die Einwohner
+                    let bundesland;                                                 // temporäre Variable für das Bundesland
+                    cities.forEach( ( city ) => {                                 // Falls die Stadt des Nutzers
+                        if (city.stadtname === user.wohnort) {                          // in der Liste der Städte vorkommt
+                            einwohnerzahl = city.einwohnerzahl;                         // sollen die temporären Variablen
+                            bundesland = city.bundesland;                           // mit den Werten aus der Liste gefüllt werden.
+                        }
+                    });
+                                                                                    // Ausgabe aller Daten
+                    console.log(`Vorname: ${user.vorname}\nNachname: ${user.nachname}\nE-Mail: ${user.email}\nWohnort: ${user.wohnort}\nEinwohnerzahl: ${einwohnerzahl}\nBundesland: ${bundesland}\n-----`);
+                })
+
+            });
+        });
+    } catch ( err ) {                                                                // Bei Fehler
+        console.error( err );                                                        // Abbruch und Melden
+    }
+}
---
+++ Dokumente/Joel Mai/Übungsaufgaben/Aufgabe_3/cities_module.js
@@ -0,0 +1,102 @@
+
+const fs = require ('fs');
+const citiesFile = './cities.json';
+
+// Exportierte Funktion getCities liefert die gelisteten Städte
+exports.getCities = () => {
+    return new Promise ( ( resolve , reject ) => {                  // Neues Promise 
+        try { 
+            fs.readFile( citiesFile , 'utf8' , ( err , data ) => {  // Einlesen des JSON-Files
+                if ( err ) reject( err );                           // Bei Fehler Abbruch
+    
+                var list = [];                                      // Anlegen der auzugebenden Liste
+                JSON.parse( data ).forEach(( city ) => {            // Für jedes JSON Objekt
+                    list.push( city.stadtname );                         // Einfügen in die Liste
+                });
+    
+                console.log(`All Cities: ${list.join(', ')}`);      // Ausgeben der Liste
+    
+                resolve();                                          // Promise einlösen
+            });
+        } catch ( err ) {                                           // oder bei Fehler -
+            reject( err );                                          // Abbrechen und Melden
+        }
+    });
+};
+
+// Exportierte Funktion findCity liefert die gesuchte Stadt
+exports.findCity = ( stadtname ) => {
+    return new Promise( ( resolve , reject ) => {                   // Neues Promise 
+        try {
+            fs.readFile( citiesFile , 'utf8' , ( err , data ) => {  // Einlesen des JSON-Files
+                if ( err ) reject( err );                           // Bei Fehler Abbruch
+        
+                var found = false;                                  // Variableninitialisierung auf Standard False
+                JSON.parse( data ).forEach( ( city ) => {           // Einlesen des JSON Files
+                    if ( city.stadtname === stadtname ) found = true;         // Jedes JSON Objekt durch gehen und Vergleichen
+                });                                                 // Ob es die gesuchte Stadt ist und dann auf true setzen
+                                                                    // Ausgabe auf der Console
+                if ( found ) console.log (`The searched City '${stadtname}' was found.`);
+                else console.log (`The searched City '${stadtname}'could not be found.`);
+    
+                resolve();                                          // Promise einlösen
+            });                 
+        } catch ( err ) {                                           // oder bei Fehler -
+            reject( err );                                          // Abbrechen und Melden
+        }
+    } );
+}
+
+// Exportierte Funktion deleteCity löscht die gewünschte Stadt
+exports.deleteCity = ( stadtname ) => {
+    return new Promise( ( resolve , reject ) => {                   // Neues Promise 
+        try {
+            fs.readFile( citiesFile , 'utf8' , ( err, data ) => {   // Einlesen des JSON-Files
+                if ( err ) reject( err );                           // Bei Fehler Abbruch
+        
+                var cities = JSON.parse( data );                    // Einlesen des JSON Files
+                cities.forEach( ( city , index , list ) => {        // Für jedes JSON Objekt
+                    if ( city.stadtname === stadtname ) list.splice( index , 1 );
+                });                                                 // Falls Objekt equals der gesuchten Stadt
+                                                                    // Objekt aus der Liste entfernen
+                var json = JSON.stringify( cities , null , 4 );     // das JSON-File wieder schreiben 
+                fs.writeFile( citiesFile , json , 'utf8' , ( err ) => {
+                    if ( err ) reject( err );                       // Bei Fehler Abbruch
+    
+                    console.log(`The City '${stadtname}' is now deleted.`);
+                                                                    // Ausgabe
+                    resolve();                                      // Promise einlösen
+                });
+            });
+        } catch ( err ) {                                           // oder bei Fehler -
+            reject( err );                                          // Abbrechen und Melden
+        }   
+    });
+}
+
+// Exportierte Funktion addCity fügt die gewünschte Stadt dem JSON-File hinzu
+exports.addCity = ( pstadtname , peinwohnerzahl , pbundesland ) => {
+    return new Promise( ( resolve , reject ) => {                   // Neues Promise 
+        try {
+            fs.readFile( citiesFile , 'utf8' , ( err , data ) => {  // Einlesen des JSON-Files
+                if ( err ) reject( err );                           // Bei Fehler Abbruch
+    
+                var cities = [];                                    // Anlegen des temporären Arrays
+                cities = JSON.parse( data );                        // JSON Objekte hinzufügen
+    
+                cities.push({stadtname: pstadtname, einwohnerzahl: peinwohnerzahl, bundesland: pbundesland});
+                                                                    // Neues Objekt anhängen
+                var json = JSON.stringify( cities , null , 4 );     // In JSON File umwandeln
+                fs.writeFile( citiesFile , json , 'utf8' , ( err ) => {
+                    if ( err ) reject( err );                       // File schreiben
+                                                                    // und bei Fehler abbrechen
+                    console.log(`The City '${pstadtname}' was succsessfully appended.`);
+                                                                    // Ausgabe
+                    resolve();                                      // Promise einlösen
+                });
+            }); 
+        } catch ( err ) {                                           // oder bei Fehler -
+            reject( err );                                          // Abbrechen und Melden
+        }   
+    });
+}
