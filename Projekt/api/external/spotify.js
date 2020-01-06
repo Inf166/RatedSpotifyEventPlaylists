59d1d7c3d0e59108f969848ca79e4529ea337b6b
Moritz D <52575034+inf155@users.noreply.github.com>
2020-01-06 19:39:07 +0100

    return audio features + uri check

--- Projekt/api/external/spotify.js
+++ Projekt/api/external/spotify.js
@@ -5,6 +5,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 
 // https://open.spotify.com/track/TRACKID?XYZ
 // spotify:track:TRACKID
+const REGEX = /^(https:\/\/open.spotify.com\/track\/|spotify:track:)([a-zA-Z0-9]+)(.*)$/;
 
 module.exports = function() {
     this.spotifyApi = new SpotifyWebApi({
@@ -27,28 +28,61 @@ module.exports = function() {
         });
     }
 
-    this.getTrack = function(trackID) {
-        this.getAccessToken().then(() => {
-            this.spotifyApi.getTrack(trackID).then(data => {
-                let trackInfo = data.body;
-                let name = trackInfo.name;
-                let artists = trackInfo.artists;
-                let duration = trackInfo.duration_ms;
-                let popularity = trackInfo.popularity;
-                console.log('Got Track Info.');
+    this.getTrack = function(trackURI) {
+        return new Promise((resolve, reject) => {
+            let validURI = trackURI.match(REGEX);
+            if (validURI) {
+                let trackID = validURI[2];
 
-                this.spotifyApi.getAudioFeaturesForTrack(trackID).then(data => {
-                    let audioFeatures = data.body;
-                    let danceability = audioFeatures.danceability;
-                    console.log('Got Audio Features.');
+                this.getAccessToken().then(() => {
+                    this.spotifyApi.getTrack(trackID).then(data => {
+                        let trackInfo = data.body;
+                        let name = trackInfo.name;
+                        let artist = trackInfo.artists[0] || 'Unknown';
+                        let duration_ms = trackInfo.duration_ms;
+                        let popularity = trackInfo.popularity;
+        
+                        this.spotifyApi.getAudioFeaturesForTrack(trackID).then(data => {
+                            let audioFeatures = data.body;
+                            let acousticness = audioFeatures.acousticness;
+                            let danceability = audioFeatures.danceability;
+                            let energy = audioFeatures.energy;
+                            let instrumentalness = audioFeatures.instrumentalness;
+                            let liveness = audioFeatures.liveness;
+                            let loudness = audioFeatures.loudness;
+                            let speechiness = audioFeatures.speechiness;
+                            let valence = audioFeatures.valence;
+                            let tempo = audioFeatures.tempo;
+        
+                            let trackData = {
+                                name: name,
+                                artist: artist,
+                                duration_ms: duration_ms,
+                                popularity: popularity,
+                                acousticness: acousticness,
+                                danceability = danceability,
+                                energy = energy,
+                                instrumentalness = instrumentalness,
+                                liveness = liveness,
+                                loudness = loudness,
+                                speechiness = speechiness,
+                                valence = valence,
+                                tempo = tempo
+                            };
+    
+                            resolve(trackData);
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    }).catch(err => {
+                        reject(err);
+                    });
                 }).catch(err => {
-                    console.error(err);
+                    reject(err);
                 });
-            }).catch(err => {
-                console.error(err);
-            });
-        }).catch(err => {
-            console.error(err);
-        })
+            } else {
+                reject('Invalid Track ID');
+            }
+        });
     }
 }
