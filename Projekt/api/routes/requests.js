23e503758cc0dad8e456d6cf3b28d6f2b53245bd
Moritz D <52575034+inf155@users.noreply.github.com>
2020-01-07 16:39:03 +0100

    einbindung des spotify handlers in die request route
    Commits werden absofort auf Deutsch gehandhabt.

--- Projekt/api/routes/requests.js
+++ Projekt/api/routes/requests.js
@@ -1,21 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
+const spotifyWebAPI = require('../../dependencies/spotify/spotify');
+const spotify = new spotifyWebAPI();
+
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'GET Response for /requests'
+        message: 'OK',
+        requests: []
     });
 });
 
 router.post('/', (req, res, next) => {
     if (track = req.body.track) {
-        res.status(201).json({
-            message: 'Request Created.',
-            track: track
+        spotify.getAccessToken().then(() => {
+            spotify.getTrack(track).then(data => {
+                res.status(201).json({
+                    message: 'Created',
+                    track: data
+                });
+            }).catch(err => {
+                if (err == 'invalidTrackID' || err.message == 'Bad Request') {
+                    res.status(400).json({
+                        message: 'Bad Request',
+                        error: 'Invalid Track ID'
+                    });
+                } else {
+                    res.status(500).json({
+                        message: 'Internal Server Error',
+                        error: err.message
+                    });
+                }
+            });
+        }).catch(err => {
+            res.status(500).json({
+                message: 'Internal Server Error',
+                error: err
+            });
         });
     } else {
         res.status(400).json({
-            message: 'Bad Request. (Missing Property: track)'
+            message: 'Bad Request',
+            error: 'Missing Property: track'
         });
     }
 });
